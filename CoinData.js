03a51a3792bd9155d28d954f744c2eb06e0e2686
semanticist14 <77091058+semanticist14@users.noreply.github.com>
2021-01-31 03:10:31 +0900

    1

---
+++ CoinData.js
@@ -0,0 +1,16 @@
+function response(
+    room,
+    msg,
+    sender,
+    isGroupChat,
+    replier,
+    imageDB,
+    packageName
+  ){
+    // 코인 가격들 불러오기
+    if(msg.indexOf("/코인") != -1){
+        var upbitBTC = org.jsoup.jsoup.connect(
+            ""
+        )
+    }  
+  }
---
+++ SearchData.js
@@ -0,0 +1,11 @@
+function response(
+    room,
+    msg,
+    sender,
+    isGroupChat,
+    replier,
+    imageDB,
+    packageName
+  ){
+      var data = org.jsoup.
+  }
