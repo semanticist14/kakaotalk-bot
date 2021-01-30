ff5e558c3c3d1c125c4cd8e05c7a5934ebeb123c
semanticist14 <77091058+semanticist14@users.noreply.github.com>
2021-01-31 01:35:52 +0900

    1

--- coin.js
+++
@@ -1,7 +0,0 @@
-function response(room, msg, sender, isGroupChat, replier, imageDB, packageName){
-    if(room == "coin2"){
-        if(msg == "test"){
-            replier.reply("답장하는 법1");
-        }
-    }
-}
